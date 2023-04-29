4b0e45603f3d7393e2144c40273757c45d719e50
Anna-Edel <125572175+Anna-Edel@users.noreply.github.com>
2023-04-29 12:40:01 +0200

    Create best_superhero.py
    add Working with the requests library

---
+++ Working with the 'requests' library, 'http', 'requests'/best_superhero.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def most_intelligence_hero(self):
+    url = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
+    response = requests.get(url)
+    resp = response.json()
+    count_intel = 0
+    for hero in resp:
+        if hero['name'] in heroes_list:
+            hero_intel = hero['powerstats']['intelligence']
+            if hero_intel > count_intel:
+                count_intel = hero_intel
+                intelligence_dict = hero['name']
+    print(intelligence_dict)
+
+
+heroes_list = ['Hulk', 'Captain America', 'Thanos']
+
+if __name__ == '__main__':
+    most_intelligence_hero(heroes_list)
